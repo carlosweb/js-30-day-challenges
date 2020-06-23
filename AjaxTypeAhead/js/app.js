5d42165d454254e8e7181e22f139a3dde66d09fd
carlosweb <kaduinfo@gmail.com>
2020-06-22 23:20:48 -0300

    finished the app

--- AjaxTypeAhead/js/app.js
+++ AjaxTypeAhead/js/app.js
@@ -1,5 +1,8 @@
 
 const endPoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+const searchInput = document.querySelector('.search')
+const suggestions = document.querySelector('.suggestions')
+
 
 const cities = [];
 
@@ -14,11 +17,63 @@ function findMatches(wordToMatch, cities){
     })
 }
 
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
 function displayMatches() {
-    console.log(this.value)
+const matchArray = findMatches(this.value, cities)
+const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi')
+    const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`)
+    const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`)
+    return `
+    <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+    </li>
+    
+    `
+}).join('')
+suggestions.innerHTML = html
 }
 displayMatches()
-const searchInput = document.querySelector('.search')
-const seggestions = document.querySelector('.suggestions')
+
+
 
 searchInput.addEventListener('change', displayMatches)
+searchInput.addEventListener('keyup', displayMatches)
+
+
+
+
+// let counter = 0
+// let score = 50;
+
+// function teste() {
+//     const timer = setInterval(() => {
+//         if(counter === score) {
+//             clearInterval(timer)
+//         }
+//         document.querySelector('#counterNum').textContent = `${counter}`
+//         counter++
+//     }, 50);
+// }
+
+// window.addEventListener('Event', teste)
+
+
+
+
+// const url = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json"
+
+// const teste = async () => {
+//     const res = await fetch(url)
+//     const data = await res.json()
+//     console.log(data)
+// }
+
+// teste()
+
+
