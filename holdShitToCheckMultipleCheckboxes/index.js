161d857083b570c7bc60284a1aeabb297d5b785b
carlosweb <kaduinfo@gmail.com>
2020-06-26 19:12:14 -0300

    Finished app

--- holdShitToCheckMultipleCheckboxes/index.js
+++ holdShitToCheckMultipleCheckboxes/index.js
@@ -1,1 +1,21 @@
 const  checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+let lastChecked
+
+
+function handleCheck(e) {
+    let inBetwen = false
+    if (e.shiftKey && this.checked){
+
+        checkboxes.forEach(checkbox => {
+            if(checkboxes === this || checkbox === lastChecked) {
+                inBetwen = !inBetwen
+            }
+            if(inBetwen) {
+                checkbox.checked = true
+            }
+        })
+    }
+    lastChecked = this
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
